e82f9534795d8d765a14efc183e56556cbed6b11
lolski <soulmagic13@gmail.com>
2014-07-07 22:38:43 +0700

    truncate.sh now remove all files in workflow_results and job_running by default

--- truncate.sh
+++ truncate.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
 mysql -u weevilscout --password=hopipola -e  'use weevilscout; truncate table geolocations; truncate table runqueue;'
+rm -rf public/workflow_results/* && rm -rf public/job_running/*
